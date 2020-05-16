03023cbbd1f8f2f93063aa4352ad228b32ee1c2e
Robin177 <39666335+Robin177@users.noreply.github.com>
2020-05-17 00:15:34 +0800

    Update test_contact.py

--- scripts/test_contact.py
+++ scripts/test_contact.py
@@ -7,9 +7,6 @@ class TestContact:
     # @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
     @allure.step(title="登录的测试脚本")
     def test_login(self):
-        allure.attach("输入用户名", "测试输入了，hello，1，str")
-        print("输入用户名")
-        self.xxx_page.input_login_edit_text()
         allure.attach("输入密码", "输入密码的描述")
         print("输入密码")
         allure.attach("登录", "登录的描述")
